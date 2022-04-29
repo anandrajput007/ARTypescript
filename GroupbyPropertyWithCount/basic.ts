8ae70335a63326e80eea88544630e422a9ae728e
Anand Rajput <anandrajput007@gmail.com>
2022-04-30 00:14:38 +0530

    naming convention

--- GroupbyPropertyWithCount/basic.ts
+++ GroupbyPropertyWithCount/basic.ts
@@ -2,14 +2,13 @@ import { Products } from './model';
 import { mockProduct } from './mock-product';
 
 let productCount: Products[] = [];
-productCount = Object.values(mockProduct.reduce((data, {Type}) => {
-    if(data[Type] === undefined) {
-        data[Type] = {Type: Type, occurrence: 1};
+productCount = Object.values(mockProduct.reduce((Product, {Type}) => {
+    if(Product[Type] === undefined) {
+        Product[Type] = {Type: Type, occurrence: 1};
     } else {
-        data[Type].occurrence++;
+        Product[Type].occurrence++;
     }
-    return data;
-
+    return Product;
 },{}));
 
 console.log(productCount);
